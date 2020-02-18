3ab1177b3d2c52b0498b8a61abceca2fba00318c
Mazter707 <mazter707@gmail.com>
2020-02-18 14:59:38 -0700

    Principal menu
    Se creo el diseno del menu principal

--- app/src/main/java/jesus/com/mazter707/fress/MainActivity.java
+++ app/src/main/java/jesus/com/mazter707/fress/MainActivity.java
@@ -1,13 +1,40 @@
 package jesus.com.mazter707.fress;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView mBbottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBbottomNavigation = (BottomNavigationView) findViewById(R.id.bottomNavigation);
+
+        mBbottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                if (menuItem.getItemId()== R.id.menu_inventory){
+
+                }
+
+                if (menuItem.getItemId()== R.id.menu_credit){
+
+                }
+
+                if (menuItem.getItemId()== R.id.menu_sales){
+
+                }
+                
+                return true;
+            }
+        });
     }
 }
