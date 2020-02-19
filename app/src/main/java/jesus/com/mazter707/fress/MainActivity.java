4887d434b2158e9dfc5db58c56ae1e653eb10009
Mazter707 <mazter707@gmail.com>
2020-02-18 17:08:54 -0700

    BottomNavigation implementation
    Se ha implementado la funcionalidad de la barra de inferior de navegacion.

--- app/src/main/java/jesus/com/mazter707/fress/MainActivity.java
+++ app/src/main/java/jesus/com/mazter707/fress/MainActivity.java
@@ -2,9 +2,15 @@ package jesus.com.mazter707.fress;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+
+import jesus.com.mazter707.fress.fragments.CreditFragment;
+import jesus.com.mazter707.fress.fragments.InventoryFragment;
+import jesus.com.mazter707.fress.fragments.SalesFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,19 +28,28 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
                 if (menuItem.getItemId()== R.id.menu_inventory){
+                    showSelectedFragment(new InventoryFragment());
 
                 }
 
                 if (menuItem.getItemId()== R.id.menu_credit){
+                    showSelectedFragment(new CreditFragment());
 
                 }
 
                 if (menuItem.getItemId()== R.id.menu_sales){
+                    showSelectedFragment(new SalesFragment());
 
                 }
 
                 return true;
             }
         });
+    }
+
+    private void showSelectedFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .commit();
     }
 }
